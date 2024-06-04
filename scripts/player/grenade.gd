d445745a0ae147e99a091a13ddbb687c05f3815f
Joshua Stanford <stanjosh@gmail.com>
2024-06-03 22:36:30 -0500

    fixed grenade collision

--- scripts/player/grenade.gd
+++ scripts/player/grenade.gd
@@ -39,7 +39,7 @@ func _on_timer_timeout():
 
 func _on_body_shape_entered(_body_rid, body, _body_shape_index, _local_shape_index):
 	if body.has_method("take_damage"):
-		if body is Grenade and body.delay:
+		if body is Grenade:
 			apply_force(linear_velocity * angular_velocity, body.global_position)
 		else:
 			body.take_damage(4, global_rotation)
