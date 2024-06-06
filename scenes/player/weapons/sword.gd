a3fbffaffc40391b294d386dee0d890e3a65a378
Joshua Stanford <stanjosh@gmail.com>
2024-06-05 21:59:54 -0500

    added cooldown control var to sword

--- scenes/player/weapons/sword.gd
+++ scenes/player/weapons/sword.gd
@@ -4,12 +4,15 @@ signal AttackFinished
 
 @onready var player = $"../.."
 @onready var attack_range = $melee_range
-@onready var cooldown = $cooldown
-var melee_hits : int = 10
-var speed : float = 40
+@onready var cooldown_time = $cooldown
+@export var speed : float = 40
+@export_range(0.2, 1) var cooldown : float = 1
 
+func _ready():
+	cooldown_time.wait_time = cooldown
+	
 func attack(delta):
-	if not cooldown.time_left:
+	if not cooldown_time.time_left:
 		player.animate(true, delta)
 		[$blade_1, $blade_2, $blade_3].pick_random().play()
 		var mobs : Array = attack_range.get_overlapping_bodies()
@@ -21,5 +24,5 @@ func attack(delta):
 					body.take_damage(randi_range(20, 30), global_position.angle_to_point(body.global_position))
 		
 
-		cooldown.start()
+		cooldown_time.start()
 	AttackFinished.emit()
