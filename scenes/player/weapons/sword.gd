5af65a10833230b20c507a066034af47b3c5e6d0
Joshua Stanford <stanjosh@gmail.com>
2024-06-06 17:35:43 -0500

    slash effect width

--- scenes/player/weapons/sword.gd
+++ scenes/player/weapons/sword.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var hitbox = $hitbox
 @onready var cooldown_time = $cooldown
+@onready var slash : Line2D = $hitbox/SlashEffect
 @export_range(1, 2) var melee_range : float = 1
 @export var speed : float = 40
 @export_range(0.2, 1) var cooldown : float = 1
@@ -11,15 +12,20 @@ func _ready():
 	cooldown_time.wait_time = cooldown
 
 func _process(delta):
-	if $hitbox/SlashEffect.modulate.a > 0:
-		$hitbox/SlashEffect.modulate.a -= 2 * delta
+	if slash.modulate.a > 0:
+		slash.modulate.a -= 2 * delta
+	else:
+		slash.hide()
 
 
 
 func attack(delta):
 	if not cooldown_time.time_left:
 		hitbox.scale *= melee_range
-		$hitbox/SlashEffect.modulate.a = 1
+		slash.width_curve.set_point_offset(1, randf_range(0.25, 0.75))
+		slash.show()
+		slash.modulate.a = 1
+
 		[$blade_1, $blade_2, $blade_3].pick_random().play()
 		var mobs : Array = hitbox.get_overlapping_bodies()
 		if mobs:
