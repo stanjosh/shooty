276c9be6eafd2247f22e3e719333ed965e0822d6
Joshua Stanford <stanjosh@gmail.com>
2024-07-04 22:27:22 -0500

    more proto

--- scenes/effects/scripts/zap_line.gd
+++ scenes/effects/scripts/zap_line.gd
@@ -3,21 +3,24 @@ extends Node2D
 
 
 @onready var zap_line = $ZapLine
-@onready var zap = $zap
 
 
 
 func _ready():
+	visible = false
 	zap_line.add_point(get_parent().global_position)
 	zap_line.add_point(get_global_mouse_position())
 
 func add_point(position : Vector2) -> void:
 	zap_line.add_point(position)
 
+func do_zap():
+	visible = true
+
 func _process(delta):
-	
-	modulate.a -= 5 * delta
-	if modulate.a <= 0:
-		queue_free()
+	if visible:
+		modulate.a -= 5 * delta
+		if modulate.a <= 0:
+			queue_free()
 
 
--- scenes/player/weapons/lightning.gd
+++ scenes/player/weapons/lightning.gd
@@ -7,29 +7,58 @@ const ZAP_LINE = preload("res://scenes/effects/ZapLine.tscn")
 
 @onready var ray_cast_2d = $RayCast2D
 
-@export var speed : float = 120
+@export var speed : float = 820
 @export var dropoff : float = .25
 @export var heat_generated : float = 10
 @export var piercing : int = 10
 @export var damage : int = 50
 @onready var zap_sound = $ZapSound
 
+var num_targets : float = 0
 var lightning_targets : Array
-
-var target_position : Vector2
+var zap
+var zap_time : float = .015
+var zap_timer : float = zap_time
 
 func _ready():
-	target_position = get_global_mouse_position()
+	zap = ZAP_LINE.instantiate()
+	get_parent().add_child(zap)
+	var m_pos = get_global_mouse_position()
+	look_at(m_pos)
+	ray_cast_2d.target_position.y = global_position.distance_to(m_pos) * 1.2
 	zap_sound.play()
+	
 
 	
 func _physics_process(delta):
-	dropoff -= 1 * delta
-	ray_cast_2d.target_position.y = global_position.distance_to(get_global_mouse_position())
+	zap_timer -= 1 * delta
+	position += Vector2.RIGHT.rotated(rotation) * speed * delta
 	if ray_cast_2d.is_colliding():
-		if ray_cast_2d.get_collider() is Mob:
-			var body_position = ray_cast_2d.get_collider().global_position
-			hurtbox.global_position = body_position
-	
-	if dropoff <= 0:
+		var target = ray_cast_2d.get_collider()
+		if target is Mob:
+			if !lightning_targets.has(target):
+				lightning_targets.push_back(target)
+				var body = ray_cast_2d.get_collider()
+				global_position = body.global_position
+
+
+	if lightning_targets.size() >= piercing or dropoff <= 0:
+		for body in lightning_targets:
+			if body is Mob:
+				body.take_damage(snapped(damage / lightning_targets.size(), 1), Vector2.DOWN)
+				zap.add_point(body.global_position)
+		zap.add_point(global_position)
+
+		zap.do_zap()
 		queue_free()
+	
+	
+	
+	
+	dropoff -= 1 * delta
+
+func _on_hurtbox_body_entered(body):
+	var target = hurtbox.get_overlapping_bodies().pick_random()
+	if !lightning_targets.has(target):
+		lightning_targets.push_back(target)
+		zap_timer = zap_time
